c03158376a005f969f408a460b7f3a930370687a
Brett Cantrell <cantrell.brett8892@gmail.com>
2023-04-06 14:16:58 -0500

    Created Post function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { useState } from 'react';
 import DisplayPosts from './Components/DisplayPosts/DisplayPosts';
+import Post from './Components/AddPost/Post';
 
 function App() {
-  const [entries, setEntries] = useState([])
+  const [entries, setEntries] = useState([{name: "Brett", post: "Sup"}, {name: "Mike", post: "Dude"}])
   return (
     <div>
       <DisplayPosts parentPost={entries} />
+      <Post />
 
     </div>
   )
 }
 export default App;
+
+// need a create post component, look at weight tracker for this
+// refer to weigh tracker 
---
+++ src/Components/AddPost/Post.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+
+const Post = (props) => {
+
+    const [name, setName] = useState('');
+    const [post, setPost] = useState('');
+    function handleSubmit(event) {
+        event.preventDefault();
+        let newPost = {
+            name: name,
+            post: post
+        };
+    }
+    return ( 
+        <form onSubmit={handleSubmit}>
+            <label>Name</label>
+            <input type='text' value={name} onChange={(event) => setName(event.target.value)} />
+            <label>Post</label>
+            <input type='text' value={post} onChange={(event) => setPost(event.target.value)} />
+            <button type='submit'>Add</button>
+        </form>
+     );
+}
+ 
+export default Post;
+
+// import post into deisplay post call it inside mapping then pass in the entry props.entry index +1 will have error to figure out
+// like and dislike button will need to be on post component, need conditional rendering then need 2 functions
+// that change two state vars for this conditional rendering
